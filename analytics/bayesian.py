cbac800a0a8d9ae0665f98629870c119e5beba26
Smart-loto <mlegoffcozigou@gmail.com>
2026-09-16 22:10:51 +0200

    Create bayesian.py

---
+++ analytics/bayesian.py
@@ -0,0 +1,23 @@
+class NumberEvidence:
+
+    number: int
+
+    frequency:
+        observed
+        expected
+        deviation
+
+    bayesian:
+        prior
+        likelihood
+        posterior
+
+    windows:
+        short
+        medium
+        long
+
+    confidence:
+        low
+        medium
+        high
